2661b71f51dae24abec73a0f0000a43b218454ea
James Frandsen <copin123@gmail.com>
2017-02-06 21:34:17 -0600

    further commit, though it throws a 500 error when I try and submit.  will compare with tutorial to see what's going on.

--- src/main/java/com/mccodecraft/HelloSpark/Article.java
+++ src/main/java/com/mccodecraft/HelloSpark/Article.java
@@ -10,47 +10,108 @@ import java.util.Date;
  */
 public class Article {
 
-    private String title;
-    private String content;
-    private String summary;
-    private Date createdAt;
-    private Integer id;
+    private final String title;
+    private final String content;
+    private final String summary;
+    private final Date createdAt;
+    private final Integer id;
     private boolean deleted;
 
-   public Article(
-           final String title,
-           final String content,
-           final String summary,
-           final Date createdAt,
-           final Integer id,
-           final boolean deleted
-   ) {
-       this.title = title;
-       this.content = content;
-       this.summary = summary;
-       this.createdAt = createdAt;
-       this.id = id;
-       this.deleted = deleted;
-   }
 
-   public Boolean readable() {
-       return !this.deleted;
-   }
+    public static class ArticleBuilder {
+        private String title;
+        private String content;
+        private String summary;
+        private Date createdAt;
+        private Integer id;
+        private boolean deleted;
 
-   public String getCreatedAt() {
-       DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-       return dateFormat.format(this.createdAt);
-   }
+        public ArticleBuilder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
 
-   public String getEditLink() {
-       return "<a href='/article/update/" + this.id + "'>Edit</a>";
-   }
+        public ArticleBuilder setContent(String content) {
+            this.content = content;
+            return this;
+        }
 
-   public String getDeleteLink() {
+        public ArticleBuilder setSummary(String summary) {
+            this.summary = summary;
+            return this;
+        }
+
+        public ArticleBuilder setCreatedAt(Date createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public ArticleBuilder setId(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public ArticleBuilder setDeleted(boolean deleted) {
+            this.deleted = true;
+            return this;
+        }
+
+        public Article build() {
+            return new Article(this);
+        }
+
+        public ArticleBuilder getArticleBuilder() {
+            return this;
+        }
+    }
+
+
+    public Article(ArticleBuilder builder) {
+        this.title = builder.title;
+        this.content = builder.content;
+        this.summary = builder.summary;
+        this.createdAt = builder.createdAt;
+        this.id = builder.id;
+        this.deleted = builder.deleted;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Boolean readable() {
+        return !this.deleted;
+    }
+
+    public void delete() { this.deleted = true; }
+
+    public String getCreatedAt() {
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        return dateFormat.format(this.createdAt);
+    }
+
+    public String getEditLink() {
+        return "<a href='/article/update/" + this.id + "'>Edit</a>";
+    }
+
+    public String getDeleteLink() {
         return "<a href='/article/delete/" + this.id + "'>Delete</a>";
     }
 
-   public String getSummaryLink() {
-       return "<a href='/article/read/" + this.id + "'>"+ this.summary+"</a>";
-   }
+    public String getSummaryLink() {
+        return "<a href='/article/read/" + this.id + "'>" + this.summary + "</a>";
+    }
+
 }
--- src/main/java/com/mccodecraft/HelloSpark/HelloSpark.java
+++ src/main/java/com/mccodecraft/HelloSpark/HelloSpark.java
@@ -8,7 +8,6 @@ import spark.Response;
 import spark.Route;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Deque;
 
 import static spark.Spark.get;
@@ -17,6 +16,7 @@ import static spark.Spark.post;
 public class HelloSpark {
 
     public static Deque<Article> articles = new ArrayDeque<Article>();
+
     public static void main(String[] args) {
         get(new Route("/") {
             @Override
@@ -27,12 +27,150 @@ public class HelloSpark {
                 html.append("<h1").append(title).append("</h1>").append(createArticleLink);
                 html.append("<hr>");
 
-                if(HelloSpark.articles.isEmpty()) {
+                if (HelloSpark.articles.isEmpty()) {
                     html.append("<b>No articles have been posted </b>");
                 } else {
-                    for (Article article  : Hello)
+                    for (Article article : HelloSpark.articles) {
+                        if(article.readable()) {
+                            html.append(article.getTitle())
+                                    .append("<br/>")
+                                    .append(article.getCreatedAt())
+                                    .append("<br/>")
+                                    .append("Summary: " + article.getSummary())
+                                    .append("<br/>")
+                                    .append(article.getEditLink()).append(" | ").append(article.getDeleteLink())
+                                    .append("</p>");
+                        }
+
+                    }
                 }
+                return html.toString();
             }
         });
+
+        get(new Route("/article/create") {
+            @Override
+            public Object handle(Request request, Response response) {
+                StringBuilder form = new StringBuilder();
+
+                form.append("<form id='article-create-form' method='POST' action='/article/create'>")
+                        .append("Title: <input type='text' name='article-title' />")
+                        .append("<br/>")
+                        .append("Summary: <input type='text' name='article-summary' />")
+                        .append("<br/>")
+                        .append("</form>")
+                        .append("<textarea name='article-content' rows='4' cols='50' form='article-create-form'></textarea>")
+                        .append("<br/>")
+                        .append("<input type='submit' value='Publish' form='article-create-form' />");
+                return form.toString();
+            }
+        });
+
+        post(new Route("/article/create") {
+            @Override
+            public Object handle(Request request, Response response) {
+                String title = request.queryParams("article-title");
+                String summary = request.queryParams("article-summary");
+                String content = request.queryParams("article-content");
+
+                Article article = new Article.ArticleBuilder()
+                        .setTitle(title)
+                        .setSummary(summary)
+                        .setContent(content)
+                        .setId(HelloSpark.articles.size() + 1)
+                        .build();
+                HelloSpark.articles.addFirst(article);
+
+                response.status(201);
+                response.redirect("/");
+                return "";
+            }
+        });
+
+        get(new Route("/article/read:id") {
+            @Override
+            public Object handle(Request request, Response response) {
+                Integer id = Integer.parseInt(request.params(":id"));
+                StringBuilder html = new StringBuilder();
+
+                for (Article article : HelloSpark.articles) {
+                    if (id.equals(article.getId())) {
+                        html.append("<a href='/'>Home</a>").append("<p/>")
+                                .append("title: ").append(article.getTitle()).append("<br/>")
+                                .append(article.getCreatedAt())
+                                .append("<p>").append(article.getContent()).append("</p>");
+                        break;
+                    }
+                }
+                return html.toString();
+            }
+        });
+        get(new Route("/article/update/:id") {
+            @Override
+            public Object handle(Request request, Response response) {
+                Integer id = Integer.parseInt(request.params(":id"));
+                StringBuilder form = new StringBuilder();
+
+                for (Article article : HelloSpark.articles) {
+                    if (id.equals(article.getId())) {
+                        form.append("<form id='article-create-form' method='POST' action='/article/update/:id'>")
+                                .append("Title: <input type='text' name='article-title' value='").append(article.getTitle()).append("' />")
+                                .append("<br/>")
+                                .append("Summary: <input type='text' name='article-summary' value='").append(article.getSummary()).append("' />")
+                                .append("<input type='hidden' name='article-id' value='").append(article.getId()).append("' />")
+                                .append("<br/>")
+                                .append("</form>")
+                                .append("<textarea name='article-content' rows='4' cols='50' form='article-create-form'>").append(article.getContent())
+                                .append("</textarea")
+                                .append("<br/>")
+                                .append("<input type='submit' value='Update' form='article-create-form' />");
+                        break;
+                    }
+                }
+                return form.toString();
+            }
+        });
+
+        get(new Route("/article/update/:id") {
+            @Override
+            public Object handle(Request request, Response response) {
+                String title = request.queryParams("article-title");
+                String summary = request.queryParams("article-summary");
+                String content = request.queryParams("article-content");
+                Integer id = Integer.parseInt(request.queryParams("article-id"));
+
+                for (Article article : HelloSpark.articles) {
+                    if (id.equals(article.getId())) {
+                        article.delete();
+                        Article newArticle = new Article.ArticleBuilder()
+                                .setTitle(title)
+                                .setSummary(summary)
+                                .setContent(content)
+                                .setId(HelloSpark.articles.size() + 1)
+                                .build();
+                        HelloSpark.articles.addFirst(article);
+                    }
+                }
+                response.status(200);
+                response.redirect("/");
+                return "";
+            }
+        });
+
+        get(new Route("/article/delete:id") {
+            @Override
+            public Object handle(Request request, Response response) {
+                Integer id = Integer.parseInt(request.params(":id"));
+                for (Article article : HelloSpark.articles) {
+                    if (id.equals(article.getId())) {
+                        article.delete();
+                    }
+                }
+                response.status(200);
+                response.redirect("/");
+                return "";
+            }
+        });
+
     }
 }
